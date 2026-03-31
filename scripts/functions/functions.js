982ff1b680fce3e4fb9bea439044e9460ea1b289
federicoacker <acker.federico@gmail.com>
2026-03-31 13:45:44 +0200

    creata funzione isPalindrome con commenti per guidarne lo sviluppo

---
+++ scripts/functions/functions.js
@@ -0,0 +1,10 @@
+function isPalindrome (stringToCheck){ // Funzione che controlla se una stringa inserita è palindroma
+    //Innanzitutto farò un controllo per vedere se ciò che è stato inserito è una stringa
+
+    //Dopo il controllo si passerà al ciclo for che controllerà se la stringa è palindroma
+    //Sto pensando magari 1 solo ciclo, 2 iteratori, uno dall'inizio della stringa, 
+    // uno dalla fine che controllano lettere a lati opposti della stringa fino a incontrarsi a metà.
+
+    //Restituirò il risultato
+
+}
