945cbe65bd74826ff1163b07b93dec50c677dc5f
federicoacker <acker.federico@gmail.com>
2026-03-31 14:54:51 +0200

    Aggiunti commenti e logica per gestire la richiesta di pari o dispari

--- scripts/index.js
+++ scripts/index.js
@@ -15,7 +15,26 @@ else{ //Questo viene loggato solo
 
 // ESERCIZIO 2 //
 
-const MAX_VALUE = 10;
-const askEvenOrOdd = askUser("string")("Inserisci pari o dispari");
-const askNumber = askUser("number")("Inserisci un numero da 1 a 10");
+const MAX_VALUE = 10; //Massimo valore per il random number generator
+const askEvenOrOddFunction = askUserGenerator("string"); // La funzione che chiederà pari o dispari
+let isAskEvenOrOddCorrect = false; // Flag booleana per controllare che la stringa inserita sia effettivamente "pari" o "dispari"
+
+//Ora, vista l'implementazione della nostra funzione askUser, dentro askUserGenerator, noi lì facciamo solo i controlli più basilari
+//Ovvero, per le stringhe, controlliamo solo se la stringa sia vuota, per i numbers, controlliamo solo se il number è NaN
+//Quindi i controlli di coerenza, dobbiamo farli comunque noi qui nel main
+//Potrei modificare le funzioni interne in modo che ricevano anche dei parametri da utilizzare per ampliare il loro ciclo while di controllo
+//Dei prompt e fare tutti i controlli là dentro. Ma al momento non ne vale la pena visto quanto sono semplici i controlli.
+
+while(!isAskEvenOrOddCorrect)
+{
+    askEvenOrOdd = askEvenOrOddFunction("Inserisci pari o dispari");
+    if(!(askEvenOrOdd.toLowerCase() === "pari" || askEvenOrOdd.toLowerCase() === "dispari")){
+        console.log("Non hai inserito pari o dispari nel prompt, riprova");
+    }else{
+        isAskEvenOrOddCorrect = true;
+    }
+}
+
+const askNumberFunction = askUserGenerator("number");
+let askNumber = askNumberFunction("Inserisci un numero da 1 a 10");
 const generatedNumber = numberGenerator(MAX_VALUE);
